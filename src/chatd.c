b61601a3dd7e2a4ad5c8f43ce511b2d92aa191e1
moderndaymoron <ragnaringir@gmail.com>
2015-11-04 18:29:47 +0000

    me

--- src/chatd.c
+++ src/chatd.c
@@ -185,7 +185,7 @@ int main(int argc, char **argv)
 						perror("accept()");
 						exit(-1);
 				  }       char reply[512];
-                  memset(%reply, 0, sizeof(reply));
+                  memset(&reply, 0, sizeof(reply));
                     printf ("Connection from %s, port %d\n",
                       inet_ntoa(client.sin_addr), ntohs(client.sin_port));
                   int fd, err;
